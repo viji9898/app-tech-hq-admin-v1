743450872dfaac9b1825b64531a08e4ef2b4d69f
viji9898 <vijitha.wijesuriya@gmail.com>
2022-08-22 14:22:13 +0300

    add-netlify-identity

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,9 +1,21 @@
 import { Link } from "react-router-dom";
 
+// Auth Control
+import netlifyIdentity from "netlify-identity-widget";
+
 export const LandingPage = () => {
+  netlifyIdentity.init({});
+
   return (
     <div style={{ margin: "15px" }}>
       <p>tech-hq-admin Console</p>
+      <button
+        onClick={() => {
+          netlifyIdentity.open();
+        }}
+      >
+        Login
+      </button>
       <ul>
         <li>
           <Link
---
+++ src/utils/authContext.js
@@ -0,0 +1,2 @@
+import { createContext, useState, useEffect } from "react";
+import netlifyIdentity from "netlify-identity-widget";
